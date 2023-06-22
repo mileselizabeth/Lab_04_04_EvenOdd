6079c2d35f5319705016484b51b90b2d3f8b4aff
mileselizabeth <mileseh@mail.uc.edu>
2023-06-22 01:31:59 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        int numToExamine = 4;
+        int modulo = numToExamine % 2;
+        System.out.println( " The number " + numToExamine + " is even because the remainder of " + numToExamine + " divided by 2 is " + modulo);
+        numToExamine = 3;
+        modulo = numToExamine % 2;
+        System.out.println( " The number " + numToExamine + " is odd because the remainder of " + numToExamine + " divided by 2 is " + modulo);
+    }
+}
